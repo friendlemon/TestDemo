44235fbee54e0284e49cf8938c8b8d9b947cd3b7
yjsong <yjsong66@163.com>
2018-10-30 01:56:10 +0800

    Update src/main/java/cn/itcast/pojo/fdfedfd.java

--- src/main/java/cn/itcast/pojo/fdfedfd.java
+++ src/main/java/cn/itcast/pojo/fdfedfd.java
@@ -1,5 +1,9 @@
 package cn.itcast.pojo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @description:
  * @author: yjsong
@@ -10,6 +14,8 @@ public class fdfedfd {
         String s = "ff";
         String r = "ff";
         String k = "ff";
-
+        ArrayList<Object> list = new ArrayList<>();
+        List<ArrayList<Object>> arrayLists = Arrays.asList(list);
+        
     }
 }
